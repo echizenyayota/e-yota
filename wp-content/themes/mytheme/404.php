480da46bdc19578dfd71b9d4879fe20143500ba8
echizenyayota <virtualmisso30@gmail.com>
2017-03-07 16:08:17 +0900

    esc_urlの適用

--- wp-content/themes/mytheme/404.php
+++ wp-content/themes/mytheme/404.php
@@ -8,7 +8,7 @@
   <div class="contents">
     <h2>404 not found!</h2>
       <p>申し訳ありません、お探しの記事はございません。</p>
-      <p><a href="<?php echo home_url(); ?>">トップページへ戻る</a></p>
+      <p><a href="<?php echo esc_url( home_url() ); ?>">トップページへ戻る</a></p>
   </div>
   <div class="sub">
     <?php get_sidebar(); ?>
