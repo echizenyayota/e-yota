85673b95321944b2aab3377ff5ec5742d4aa7102
echizenyayota <virtualmisso30@gmail.com>
2017-03-28 16:28:45 +0900

    get_post_thumbnail_idでIDが取れているかどうかのチェック

--- wp-content/themes/mytheme/single.php
+++ wp-content/themes/mytheme/single.php
@@ -48,7 +48,12 @@
           <div class="catch">
             <?php the_post_thumbnail( 'large' ); ?>
             <p class="wp-caption-text">
-              <?php echo get_post( get_post_thumbnail_id() )->post_excerpt; ?>
+              <?php
+                $post_thumbnail_id = get_post_thumbnail_id();
+                if($post_thumbnail_id) {
+                  echo get_post( $post_thumbnail_id )->post_excerpt;
+                 }
+                ; ?>
             </p>
           </div>
         <?php endif; ?>
