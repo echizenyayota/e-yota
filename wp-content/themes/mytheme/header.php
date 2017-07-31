4f40af881147d69822ceb24089fd30736390bff0
echizenya <h.fqui35@gmail.com>
2017-07-31 17:50:01 +0900

    header.phpにGoogle Analyticsのスクリプトコードを貼り付け

--- wp-content/themes/mytheme/header.php
+++ wp-content/themes/mytheme/header.php
@@ -87,6 +87,16 @@
     <meta name="<?php echo esc_attr( 'twitter:card' ); ?>" content="<?php echo esc_attr( 'summary_large_image' ); ?>">
     <link rel="shortcut icon" href="<?php echo get_template_directory_uri(); ?>/ecoteki_favicon.ico" />
 
+    <!-- Google Analytics-->
+    <script>
+      (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+      (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+      m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+      })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+      ga('create', 'UA-47572268-1', 'auto');
+      ga('send', 'pageview');
+      </script>
   <?php wp_head(); ?>
 </head>
 <body <?php body_class(); ?>>
