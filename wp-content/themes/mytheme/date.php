e98d3ed59356d8ba16082bd0054255e6fa931786
echizenya <h.fqui35@gmail.com>
2017-08-25 17:50:25 +0900

    date.phpにGoogle Adsenseの追加

--- wp-content/themes/mytheme/date.php
+++ wp-content/themes/mytheme/date.php
@@ -2,7 +2,19 @@
 <div class="sub-header">
   <div class="google-mobile">
     <div class="ad1">
-      <p><img src="" height="50" width=320></p>
+      <p>
+        <!-- <img src="" width=320 height="50" > -->
+        <script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+          <!-- e-yota_mobile -->
+          <ins class="adsbygoogle"
+               style="display:block"
+               data-ad-client="ca-pub-4555449890300852"
+               data-ad-slot="4080387032"
+               data-ad-format="auto"></ins>
+          <script>
+          (adsbygoogle = window.adsbygoogle || []).push({});
+        </script>
+      </p>
     </div>
   <div>
 <div class="bread">
