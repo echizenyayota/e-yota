5f11b76bae0dd4aa1a778edadeb80915f619c3af
echizenya <h.fqui35@gmail.com>
2018-07-02 17:25:33 +0900

    スマホでブログサイトを表示したときのナビゲーション メニューの動作を変更

--- wp-content/themes/mytheme/navbtn.js
+++ wp-content/themes/mytheme/navbtn.js
@@ -3,3 +3,11 @@ jQuery (function() {
     jQuery ("#mainmenu").slideToggle();
   });
 });
+
+// スマホのナビメニューを正確に動かせるようにする
+jQuery('ul#menu-menu li:has(.sub-menu) > a').click(function() {
+  if (jQuery('#navbtn').css('display') != 'none') {
+    jQuery('.sub-menu', jQuery(this).parent()).toggle();
+    return false;
+  }
+});
