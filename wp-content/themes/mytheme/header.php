1eed8dc1b66d97841f1ce7f8b8267cbb9008edc6
echizenya <h.fqui35@gmail.com>
2018-07-02 16:11:09 +0900

    ウィジェット「なかのひと」のアイコンのマージン調整とmetaタグの修正

--- wp-content/themes/mytheme/header.php
+++ wp-content/themes/mytheme/header.php
@@ -2,7 +2,7 @@
 <html <?php language_attributes(); ?>>
 <head prefix="og: http://ogp.me/ns#">
   <meta charset="utf-8">
-  <meta name="viewport" content="width=device-width" initial-scale="1.0">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css">
   <link rel="stylesheet" href="http://fonts.googleapis.com/earlyaccess/notosansjp.css">
 
