97b700ebc40bf166854f6e539327276e1c9c8b08
echizenya <h.fqui35@gmail.com>
2019-02-16 14:05:32 +0900

    single.phpのアフィリエイト広告を変更

--- wp-content/themes/mytheme/single.php
+++ wp-content/themes/mytheme/single.php
@@ -140,9 +140,9 @@
             <?php the_tags('<ul><li>', '</li><li>','</li></ul>'); ?>
           </div>
           <div class="sakura-vps-ad">
-              <a href="https://px.a8.net/svt/ejp?a8mat=2ZS4XD+E39C6Q+D8Y+6L9O1" target="_blank" rel="nofollow">
-              <img border="0" width="728" height="90" alt="" src="https://www28.a8.net/svt/bgt?aid=181031665852&wid=001&eno=01&mid=s00000001717001107000&mc=1"></a>
-              <img border="0" width="1" height="1" src="https://www13.a8.net/0.gif?a8mat=2ZS4XD+E39C6Q+D8Y+6L9O1" alt="">
+              <a href="https://px.a8.net/svt/ejp?a8mat=2ZS4XD+EEKKOI+D8Y+C7TC1" target="_blank" rel="nofollow">
+                <img border="0" width="728" height="90" alt="" src="https://www22.a8.net/svt/bgt?aid=181031665871&wid=001&eno=01&mid=s00000001717002052000&mc=1"></a>
+                <img border="0" width="1" height="1" src="https://www15.a8.net/0.gif?a8mat=2ZS4XD+EEKKOI+D8Y+C7TC1" alt="">
           </div>
         <?php wp_reset_postdata(); ?>
 
