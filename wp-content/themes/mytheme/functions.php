ac2524daca0c4b2c60bb6151226d5c71d23ac271
echizenya <h.fqui35@gmail.com>
2019-02-21 13:45:10 +0900

    style.cssの圧縮ファイルを子テーマで適用

--- wp-content/themes/mytheme/functions.php
+++ wp-content/themes/mytheme/functions.php
@@ -1,10 +1,11 @@
 <?php
 
 // 子テーマの読み込み
-// add_action( 'wp_enqueue_scripts', 'theme_enqueue_styles' );
-// function theme_enqueue_styles() {
-//     wp_enqueue_style( 'parent-style', get_template_directory_uri() . '/style.css' );
-// }
+add_action( 'wp_enqueue_scripts', 'theme_enqueue_styles' );
+function theme_enqueue_styles() {
+    wp_enqueue_style( 'parent-style', get_template_directory_uri() . '/style.css' );
+    wp_enqueue_style( 'child-style', get_stylesheet_directory_uri() . '/style.css', array('parent-style'));
+}
 
 // 抜粋の文字数
 function my_length($length) {
