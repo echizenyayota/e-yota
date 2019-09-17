5649d77aca538aec3c2f5f1a4a031e7c5d5103f8
echizenya <h.fqui35@gmail.com>
2019-09-17 14:22:25 +0900

    Webアイコンの除去（コメントアウトされていたSNSアイコンの除去）

--- wp-content/themes/mytheme/single.php
+++ wp-content/themes/mytheme/single.php
@@ -106,14 +106,12 @@
             <li>
               <a href="https://twitter.com/intent/tweet?text=<?php echo urlencode( get_the_title() . ' - ' . get_bloginfo('name') ); ?>&amp;url=<?php echo urlencode( get_permalink() ); ?>&amp;via=echizenya_yota"
             	onclick="window.open(this.href, 'SNS', 'width=500, height=300, menubar=no, toolbar=no, scrollbars=yes'); return false;" class="share-tw">
-                <!-- <i class="fab fa-twitter"></i> -->
                 <span>Twitter</span> ツイート
               </a>
             </li>
             <li>
               <a href="http://www.facebook.com/share.php?u=<?php echo urlencode( get_permalink() ); ?>"
             	onclick="window.open(this.href, 'SNS', 'width=500, height=500, menubar=no, toolbar=no, scrollbars=yes'); return false;" class="share-fb">
-                <!-- <i class="fab fa-facebook-f"></i> -->
                 <span>Facebook</span> シェア
               </a>
             </li>
