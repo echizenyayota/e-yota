014ba74f666cc826c913dd86bf78f86416eb7ed1
echizenya <h.fqui35@gmail.com>
2019-12-27 00:26:41 +0900

    人気記事の変更（ゲゲゲの女房からおしんに変更）

--- wp-content/themes/mytheme/sidebar.php
+++ wp-content/themes/mytheme/sidebar.php
@@ -28,7 +28,7 @@
 
 
 <?php
-  $the_slug = 'gegege_no_nyobo';
+  $the_slug = 'oshin';
   $myposts = get_posts( array(
     'category_name' => $the_slug,
     'post_type' => 'post',
@@ -89,7 +89,7 @@
 
 <!-- 人気の記事 -->
 <aside class="mymenu mymenu-thumb">
-  <h2>ゲゲゲの女房人気記事</h2>
+  <h2>おしんの人気記事</h2>
   <ul>
     <?php foreach($myposts as $post): setup_postdata($post); ?>
       <li>
