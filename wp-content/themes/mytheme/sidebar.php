956b629bbc583b0792e50a0c1245a1061b41f2f1
echizenya <h.fqui35@gmail.com>
2020-06-08 08:34:23 +0900

    サイドバーの見出しを「大家さんと僕」に変更

--- wp-content/themes/mytheme/sidebar.php
+++ wp-content/themes/mytheme/sidebar.php
@@ -28,7 +28,7 @@
 
 
 <?php
-  $the_slug = 'hanekonma';
+  $the_slug = 'ohyasan_to_boku';
   $myposts = get_posts( array(
     'category_name' => $the_slug,
     'post_type' => 'post',
@@ -89,7 +89,7 @@
 
 <!-- 人気の記事 -->
 <aside class="mymenu mymenu-thumb">
-  <h2>朝ドラ再放送「はね駒」</h2>
+  <h2>NHKアニメ「大家さんと僕」</h2>
   <ul>
     <?php foreach($myposts as $post): setup_postdata($post); ?>
       <li>
