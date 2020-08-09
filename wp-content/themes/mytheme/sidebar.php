9bdae4fcb6ce9dd99875b49afde80e013da88699
echizenya <h.fqui35@gmail.com>
2020-08-09 11:55:27 +0900

    サイドバーの見出しと記事の変更（ボディーコーチ）

--- wp-content/themes/mytheme/sidebar.php
+++ wp-content/themes/mytheme/sidebar.php
@@ -7,7 +7,7 @@
   if ($myposts) : ?>
 
 <aside class="mymenu mymenu-large">
-  <h2>フィジカルディスタンスとソーシャルディスタンス</h2>
+  <h2>NHK「ボディーコーチ」のレシピ</h2>
   <ul>
     <?php foreach($myposts as $post):
        if ($post->object == 'post'):
@@ -28,7 +28,7 @@
 
 
 <?php
-  $the_slug = 'hanekonma';
+  $the_slug = 'body_coach';
   $myposts = get_posts( array(
     'category_name' => $the_slug,
     'post_type' => 'post',
@@ -89,7 +89,7 @@
 
 <!-- 人気の記事 -->
 <aside class="mymenu mymenu-thumb">
-  <h2>「はね駒」人気記事</h2>
+  <h2>NHK「ボディーコーチ」人気記事</h2>
   <ul>
     <?php foreach($myposts as $post): setup_postdata($post); ?>
       <li>
