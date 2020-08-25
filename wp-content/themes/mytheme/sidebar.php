854cf56d230d87a99af9e296256415181a7b4460
echizenya <h.fqui35@gmail.com>
2020-08-25 13:04:44 +0900

    サイドバー見出しと記事の変更

--- wp-content/themes/mytheme/sidebar.php
+++ wp-content/themes/mytheme/sidebar.php
@@ -7,7 +7,7 @@
   if ($myposts) : ?>
 
 <aside class="mymenu mymenu-large">
-  <h2>ダンススタジアム2020</h2>
+  <h2>同志社香里高校ダンス部 関連記事</h2>
   <ul>
     <?php foreach($myposts as $post):
        if ($post->object == 'post'):
@@ -28,7 +28,7 @@
 
 
 <?php
-  $the_slug = 'highschool_dance_club';
+  $the_slug = 'dkdc';
   $myposts = get_posts( array(
     'category_name' => $the_slug,
     'post_type' => 'post',
@@ -89,7 +89,7 @@
 
 <!-- 人気の記事 -->
 <aside class="mymenu mymenu-thumb">
-  <h2>高校ダンス部 人気記事</h2>
+  <h2>同志社香里高校ダンス部 人気記事</h2>
   <ul>
     <?php foreach($myposts as $post): setup_postdata($post); ?>
       <li>
