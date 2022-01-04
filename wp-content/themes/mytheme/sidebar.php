f947f328030486ab6f4fbde9f9d712054d2fcf62
echizenyayota <virtualmisso30@gmail.com>
2022-01-04 19:22:28 +0900

    サイドバーの見出し変更

--- wp-content/themes/mytheme/sidebar.php
+++ wp-content/themes/mytheme/sidebar.php
@@ -28,7 +28,7 @@
 
 
 <?php
-  $the_slug = 'come_come_everybody';
+  $the_slug = 'kamakuradono_13';
   $myposts = get_posts( array(
     'category_name' => $the_slug,
     'post_type' => 'post',
@@ -87,7 +87,7 @@
 
 <!-- 人気の記事 -->
 <aside class="mymenu mymenu-thumb">
-  <h2>カムカムエヴリバディ人気記事</h2>
+  <h2>鎌倉殿の13人 人気記事</h2>
   <ul>
     <?php foreach($myposts as $post): setup_postdata($post); ?>
       <li>
