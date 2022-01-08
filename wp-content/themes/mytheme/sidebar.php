d2668af2fcb433dbdf89e98f6f35c70396812cbb
echizenyayota <virtualmisso30@gmail.com>
2022-01-08 22:34:48 +0900

    サイドバー見出しの変更

--- wp-content/themes/mytheme/sidebar.php
+++ wp-content/themes/mytheme/sidebar.php
@@ -28,7 +28,7 @@
 
 
 <?php
-  $the_slug = 'come_come_everybody';
+  $the_slug = 'dousuruieyasu';
   $myposts = get_posts( array(
     'category_name' => $the_slug,
     'post_type' => 'post',
@@ -87,7 +87,7 @@
 
 <!-- 人気の記事 -->
 <aside class="mymenu mymenu-thumb">
-  <h2>カムカムエヴリバディ 人気記事</h2>
+  <h2>次の大河ドラマ どうする家康</h2>
   <ul>
     <?php foreach($myposts as $post): setup_postdata($post); ?>
       <li>
