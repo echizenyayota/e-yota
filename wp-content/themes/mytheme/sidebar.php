702d879462b8786906e628ee074e206d41624b11
echizenyayota <virtualmisso30@gmail.com>
2022-02-06 10:18:30 +0900

    サイドバー見出し変更

--- wp-content/themes/mytheme/sidebar.php
+++ wp-content/themes/mytheme/sidebar.php
@@ -7,7 +7,7 @@
   if ($myposts) : ?>
 
 <aside class="mymenu mymenu-large">
-  <h2>カムカムエヴリバディ ひなた(川栄李奈さん)編</h2>
+  <h2>ちむどんどん 2022年前期朝ドラ</h2>
   <ul>
     <?php foreach($myposts as $post):
        if ($post->object == 'post'):
@@ -28,7 +28,7 @@
 
 
 <?php
-  $the_slug = 'chimudondon';
+  $the_slug = 'mai_agare';
   $myposts = get_posts( array(
     'category_name' => $the_slug,
     'post_type' => 'post',
@@ -87,7 +87,7 @@
 
 <!-- 人気の記事 -->
 <aside class="mymenu mymenu-thumb">
-  <h2>次の朝ドラ ちむどんどん</h2>
+  <h2>舞い上がれ！ 2022年後期朝ドラ</h2>
   <ul>
     <?php foreach($myposts as $post): setup_postdata($post); ?>
       <li>
