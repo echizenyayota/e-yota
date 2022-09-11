dd1a4f45097fadde59198dbb06a104c73c24461a
echizenyayota <virtualmisso30@gmail.com>
2022-09-12 08:50:08 +0900

    サイドバー見出し変更

--- wp-content/themes/mytheme/sidebar.php
+++ wp-content/themes/mytheme/sidebar.php
@@ -7,7 +7,7 @@
   if ($myposts) : ?>
 
 <aside class="mymenu mymenu-large">
-  <h2>朝ドラ次 2022年後期 舞いあがれ！</h2>
+  <h2>朝ドラ再放送 本日も晴天なり</h2>
   <ul>
     <?php foreach($myposts as $post):
        if ($post->object == 'post'):
@@ -28,7 +28,7 @@
 
 
 <?php
-  $the_slug = 'honjitsu_mo_seitennari';
+  $the_slug = 'mai_agare';
   $myposts = get_posts( array(
     'category_name' => $the_slug,
     'post_type' => 'post',
@@ -87,7 +87,7 @@
 
 <!-- 人気の記事 -->
 <aside class="mymenu mymenu-thumb">
-  <h2>朝ドラ再放送本日も晴天なり 人気記事</h2>
+  <h2>朝ドラ2022年後期 舞いあがれ！</h2>
   <ul>
     <?php foreach($myposts as $post): setup_postdata($post); ?>
       <li>
