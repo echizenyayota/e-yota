00315150ff71b2ff4a90f19d56054da302306e15
echizenyayota <virtualmisso30@gmail.com>
2022-10-17 17:07:57 +0900

    サイドバー見出し変更

--- wp-content/themes/mytheme/sidebar.php
+++ wp-content/themes/mytheme/sidebar.php
@@ -28,7 +28,7 @@
 
 
 <?php
-  $the_slug = 'honjitsu_mo_seitennari';
+  $the_slug = 'nhk_kaigai';
   $myposts = get_posts( array(
     'category_name' => $the_slug,
     'post_type' => 'post',
@@ -87,7 +87,7 @@
 
 <!-- 人気の記事 -->
 <aside class="mymenu mymenu-thumb">
-  <h2>朝ドラ再放送 本日も晴天なり</h2>
+  <h2>NHK海外ドラマ</h2>
   <ul>
     <?php foreach($myposts as $post): setup_postdata($post); ?>
       <li>
